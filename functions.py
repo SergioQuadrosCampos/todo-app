9a8e4bc8368ff56bbd7136f988d8a9b94a52f300
Sergio Lucas Quadros Campos <SergioQuadrosCampos@users.noreply.github.com>
2023-02-05 09:48:12 -0300

    Git course

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
-def get_todos(filepath='files/todos.txt'):
+FILEPATH = "files/todos.txt'"
+
+
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath='files/todos.txt'):
-    with open('files/todos.txt', 'w') as file_local:
+    with open(FILEPATH, 'w') as file_local:
         file_local.writelines(todos_arg)
